e5c68bc486e1cde4d7ff04c5971deac36aa6c74f
Lan Anh <trantruonglananh225@gmail.com>
2025-04-03 14:30:45 +0700

    bo /slug cho dep

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 });
 
 // Lấy tất cả sản phẩm trong một danh mục theo slug
-router.get('/slug/:category', async function(req, res) {
+router.get('/:category', async function(req, res) {
   try {
     let category = await categoryModel.findOne({ slug: req.params.category });
     if (!category) {
@@ -26,7 +26,7 @@ router.get('/slug/:category', async function(req, res) {
 });
 
 // Lấy sản phẩm theo slug category và slug sản phẩm
-router.get('/slug/:category/:product', async function(req, res) {
+router.get('/:category/:product', async function(req, res) {
   try {
     let category = await categoryModel.findOne({ slug: req.params.category });
     if (!category) {
